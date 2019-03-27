aa085f1c0f3b6a8dc7bfb2aa381d9fd754985b90
Alex Koik-Cestone <akoikcestone@gmail.com>
2019-03-27 01:59:12 -0400

    Improvements to rendering / usability

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # BJ2019
 
-Do something in xeogl?
+Decided to use THREE.js
+
+v0.0.2pre
+* Added distance fog
+* Increased height of camera
+* Reduced plane size
+* Window resize now updates canvas / 3d scene
+* Added grab cursor
+* Cleaned up code
+
+v0.0.1pre
+* Got GLTF load working
+* Got basic lighting setup
+* Object now present, and rotates.
+
 
 ## Ideas
 
 Birthday cake.
-* Realistically shaded.
+* Realistically shaded - implemented for the CISSP chip at least.
 * Realistic candles.
-* xeogl.
--- scripts/2019cake.js
+++ scripts/2019cake.js
@@ -1,92 +1,106 @@
+var camera, renderer;
+
 window.onload = function() {
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var controls = new THREE.OrbitControls(camera);
-
-var renderer = new THREE.WebGLRenderer();
+    // scene and camera.
+    var scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 20;
     
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    // orbit controls
+    var controls = new THREE.OrbitControls(camera);
+    controls.enableDamping = true;
+    controls.enableKeys = true;
+    controls.maxPolarAngle = Math.PI / 2;
     
-renderer.setSize(window.innerWidth, window.innerHeight);
+    //controls.autoRotate = true;
 
-document.body.appendChild(renderer.domElement);
+    // renderer with better shadow map
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
     
+    
+    // add distance fog
+    scene.fog = new THREE.Fog(0x000000, 10, 80);
+
+    
+    // add ambient light
+    var light = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(light);    
+
+    
+    // add ground plane, receiving shadow from object that cast(s)Shadow
+    var geometry = new THREE.PlaneGeometry(150, 150, 100, 100);
+    var material = new THREE.MeshStandardMaterial({color: 0xC0C0C0, side: THREE.DoubleSide});
+
+    var plane = new THREE.Mesh(geometry, material);
+    scene.add(plane);
+    plane.position.y = -15;
+    plane.rotation.x = Math.PI / 2;
+    plane.receiveShadow = true;    
+
+    
+    // add spotlight that casts shadow onto objects that recieve it
+    var spotLight = new THREE.SpotLight(0xffffff);
+
+    spotLight.position.set(5, 20, 3);
+    spotLight.castShadow = true;
+    spotLight.penumbra = 0.5;
+    spotLight.intensity = 1;
+
+    // make higher res
+    spotLight.shadowMapWidth = 1024;
+    spotLight.shadowMapHeight = 1024;
+
+    scene.add(spotLight);
+
+    // add spotlight helper
+    //var spotLightHelper = new THREE.SpotLightHelper(spotLight);
+    //scene.add(spotLightHelper);
+        
+    
+    // add axis helper
+//        var axesHelper = new THREE.AxesHelper(15);
+//        scene.add(axesHelper);    
+    
+    
+    // load CISSP chip model
     var chip;
     
     var loader = new THREE.GLTFLoader();
     loader.load('models/cissp chip2.glb',
     function(gltf) {
         
+        // add gltf scene and make chip cast shadow from light
         scene.add(gltf.scene);
         chip = scene.getObjectByName('CisspChip');
         chip.castShadow = true;
-        console.log(chip);
-/*
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    
-*/
-        
-        var light = new THREE.AmbientLight(0xffffff, 0.5);
-        scene.add(light);
-        
-        var axesHelper = new THREE.AxesHelper(15);
-        scene.add(axesHelper);
-        
-        console.log(scene);
-        camera.position.z = 20;
-        
-        var geometry = new THREE.PlaneGeometry(500, 500, 100, 100);
-        var material = new THREE.MeshStandardMaterial({color: 0xC0C0C0, side: THREE.DoubleSide});
-        
-        var plane = new THREE.Mesh(geometry, material);
-        scene.add(plane);
-        plane.position.y = -15;
-        plane.rotation.x = Math.PI / 2;
-        plane.receiveShadow = true;
-        
-        // spotlight
-        var spotLight = new THREE.SpotLight(0xffffff);
-        
-        spotLight.position.set(5, 20, 3);
-        spotLight.castShadow = true;
-        spotLight.penumbra = 0.5;
-        spotLight.intensity = 1;
-        
-        // make higher res
-        spotLight.shadowMapWidth = 1024;
-        spotLight.shadowMapHeight = 1024;
-        
-        scene.add(spotLight);
-        
-        var spotLightHelper = new THREE.SpotLightHelper(spotLight);
-        scene.add(spotLightHelper);
         
     }, undefined, function(error) {
             console.error(error);
     });
-    
-    
-    
 
+    function animate() {
+        requestAnimationFrame(animate);
 
-function animate() {
-    requestAnimationFrame(animate);
-    
-    chip.rotation.x += 0.01;
-    chip.rotation.y += 0.01;
-    
-    controls.update();
-    
-    renderer.render(scene, camera);
-}
-animate();
+        chip.rotation.x += 0.01;
+        chip.rotation.y += 0.01;
+
+        controls.update();
+
+        renderer.render(scene, camera);
+    }
+    animate();
 }
 
+// update on resize
 window.onresize = function() {
-    
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
